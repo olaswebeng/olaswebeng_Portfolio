1a051be6ffe4136034cc230b80e8c3ebd2acf483
ahmadjalal1122 <l201305@lhr.nu.edu.pk>
2023-08-17 11:13:17 +0500

    update

--- pages/services/index.js
+++ pages/services/index.js
@@ -13,17 +13,24 @@ const Services = () => {
     <div className='h-full bg-primary/30 py-36 flex items-center'>
       <Circles/>
       <div className='container mx-auto'>
-        <div className='flex flex-col xl:flex-row gap-x-8'>
+        <div className='flex flex-col xl:flex-row gap-x-8'
+          style={{
+            maxHeight: '60vh', 
+            overflowY: 'auto',
+          }}
+        >
           {/* text */}
+           
           <div className='text-center flex xl:w-[30vw] flex-col lg:text-left  mb-4
           xl:mb-0
           '>
+
             <motion.h2 
             variants={fadeIn('up',0.3)}
             initial='hidden'
             animate='show'
             exit='hidden'
-            className='h2 xl:mt-8 my-4'
+            className='h2 xl:mt-8 my-2'
             style={{fontSize:"5vh",marginTop:'30%'}}
             >My Services
             <span className='text-accent'>.</span>
@@ -33,16 +40,20 @@ const Services = () => {
             initial='hidden'
             animate='show'
             exit='hidden'
-            className='mb-4 max-w-[400px] mx-auto lg:mx-0'>
+            className='mb-2 max-w-[400px] mx-auto lg:mx-0'>
              What I can offers to you?
               </motion.p>
+           
+
           </div>
           <motion.div
           variants={fadeIn('down',0.6)}
           initial='hidden'
           animate='show'
           exit='hidden'
-          className='w-full xl:max-w-[65%]'>
+          className='w-full xl:max-w-[65%]'
+        
+          >
           {/* Slider */}
           <ServiceSlider/>
           <div className='text-center'>
@@ -52,7 +63,6 @@ const Services = () => {
           Checkout on Fiverr
           </Link>
           </div>
-
           </motion.div>
         </div>
       </div>
