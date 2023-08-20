b48eb334b44310bf3b0a579cdcf78bade9285aab
ahmadjalal1122 <l201305@lhr.nu.edu.pk>
2023-08-20 14:25:30 +0500

    updated

--- components/WorkSlider.js
+++ components/WorkSlider.js
@@ -94,17 +94,18 @@ const WorkSlider = () => {
                       transition-all duration-300 
                       '>
                         <div className='flex items-center gap-x-2
-                        text-[13px] tracking-[0.2em]
-                        '>
+                        text-[13px] tracking-[0.2em] projectTitle
+                        ' >
                           {/* title part1 */}
                           <div className='delay-100' 
                           style={{textTransform:'uppercase'}}
-                          >{image.title}</div>
+                          >{image.title}
+                          </div>
                           {/* title part2 */}
-                          <div className='translate-y-[500%] 
+                          {/* <div className='translate-y-[500%] 
                           group-hover:translate-y-0 transition-all duration-300
                           delay-150
-                          '>PROJECT</div>
+                          '>PROJECT</div> */}
                           {/* icons */}
                           <div className='text-xl translate-y-[500%]
                           group-hover:translate-y-0 transition-all duration-300
